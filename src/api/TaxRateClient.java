8db7128baf4c6f84d38a83cd25011626e482709e
Stormi Dragon <Thegreatjinxycat@gmail.com>
2023-11-15 22:44:39 -0600

    Changed API to a better solution

--- src/api/TaxRateClient.java
+++ src/api/TaxRateClient.java
@@ -1,40 +1,64 @@
 package api;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import data.TaxRateResponse;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+
 public class TaxRateClient {
 
-    private static final String API_URL = "https://retrieveustaxrate.p.rapidapi.com/GetTaxRateByZip?zip=";
-    private static final String AUTH_HEADER = "Basic Ym9sZGNoYXQ6TGZYfm0zY2d1QzkuKz9SLw==";
-    private static final String API_KEY = "77600ecdedmsh1931c459c3b1795p1025eejsn9fa34f562588";
-    private static final String API_HOST = "retrieveustaxrate.p.rapidapi.com";
+    private static final String API_URL = "https://sales-tax-by-api-ninjas.p.rapidapi.com/v1/salestax";
+    private static final String API_KEY = "6b45072e8cmshabbcd707434ba6ap1d4fbdjsn0350b846cc80";
+    private static final String API_HOST = "sales-tax-by-api-ninjas.p.rapidapi.com";
     private final Gson gson;
 
     public TaxRateClient() {
         gson = new Gson();
     }
 
-    public TaxRateResponse getTaxRateByZip(String zipCode) {
+    private URI buildURI(String zipCode, String city, String state) {
+        StringBuilder uriBuilder = new StringBuilder(API_URL);
+
+        boolean firstParam = true;
+
+        if (zipCode != null && !zipCode.isEmpty()) {
+            uriBuilder.append(firstParam ? "?" : "&").append("zip_code=").append(zipCode);
+            firstParam = false;
+        }
+        if (city != null && !city.isEmpty()) {
+            uriBuilder.append(firstParam ? "?" : "&").append("city=").append(city);
+            firstParam = false;
+        }
+        if (state != null && !state.isEmpty()) {
+            uriBuilder.append(firstParam ? "?" : "&").append("state=").append(state);
+        }
+
+        return URI.create(uriBuilder.toString());
+    }
+
+    public TaxRateResponse getTaxRateFromZipCityState(String zipCode, String city, String state) {
         try {
             HttpClient client = HttpClient.newHttpClient();
+            URI uri = buildURI(zipCode, city, state);
+
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(API_URL + zipCode))
-                    .header("Authorization", AUTH_HEADER)
+                    .uri(uri)
                     .header("X-RapidAPI-Key", API_KEY)
                     .header("X-RapidAPI-Host", API_HOST)
                     .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            // Parse JSON response using Gson
-            return gson.fromJson(response.body(), TaxRateResponse.class);
+            List<TaxRateResponse> taxRates = gson.fromJson(response.body(), new TypeToken<List<TaxRateResponse>>(){}.getType());
+            return taxRates.isEmpty() ? null : taxRates.get(0);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
+
 
 }
